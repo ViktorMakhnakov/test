28241908161aff07245111a852fcd63a9a0185ea
ViktorMakhnakov <vityushka97@mail.ru>
2020-10-17 22:32:35 +0700

    fs

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include "stdio.h"
 
-
+int sum(int a, int b) {
+  return a+b;
+}
 
 
 int main() {
   int i;
   printf("Hello, World!");
-  scanf("%d",&i);
-  
+  scanf("%d %d",&i,&x);
+  printf("Hello, World!%d",sum(i,x)); 
+    scanf("%d",&i);   
 }
